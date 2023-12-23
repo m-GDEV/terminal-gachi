c089c2976c2e501346e410dac6fb54e985c42cb5
Musa Ahmed <musaa.ahmed7@gmail.com>
2023-12-23 02:20:42 -0500

    input and output is working!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 // System imports
-use std::env;
 use std::fs;
 
 // Serde imports
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
 
 // Struct defs
 #[derive(Serialize, Deserialize, Debug)]
@@ -30,19 +28,49 @@ struct CommandsPerDay {
     commands_run: i32,
 }
 
-fn main() -> Result<()> {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data: String =
-        fs::read_to_string("./test-data.json").expect("Should have been able to read the file");
+fn read_tamogachi(path: &str) -> Tamogachi {
+    // The so both read_to_string and from_str can "throw exceptions"
+    // Because of this i added a ".expect", this essentially
+    // tells the program to try and execute that funtion
+    // and if it can't then to "throw an exception"
+    // and exit the program. This is called a "panic"
 
+    // Also, the way expect works is that it handles
+    // Result enums. These enums have two possibilities:
+    // a generic type and an error. If what the
+    // functions mentioned above return a Result
+    // that has error "selected" it will print
+    // the message and exit. Otherwise it will
+    // just return the generic type.
+    // This essentially allows for me to assume
+    // what the functions return is the type i want
+
+    // Read json from file
+    let data: String = fs::read_to_string(path).expect("Error reading file!");
+
+    // Convert data to &str since serde wants that
     let s: &str = data.as_str();
-    // Parse the string of data into a Person object. This is exactly the
-    // same function as the one that produced serde_json::Value above, but
-    // now we are asking it for a Person as output.
-    let p: Tamogachi = serde_json::from_str(s)?;
 
-    // Do things just like with any other Rust data structure.
-    println!("{:?}", p.name);
+    // Parse the string of data into a Tamogachi object
+    let obj: Tamogachi = serde_json::from_str(s).expect("Cannot convert file to struct!");
+    return obj;
+}
 
-    Ok(())
+fn write_tamogachi(obj: Tamogachi, path: &str) {
+    let json = serde_json::to_string(&obj).expect("Cannot convert struct to json!");
+
+    fs::write(path, json).expect("Cannot save file!");
+}
+
+fn main() {
+    let file_path = "./test-data.json";
+
+    // Load data
+    let mut obj: Tamogachi = read_tamogachi(file_path);
+
+    // Modify data
+    obj.name = String::from("Musa");
+
+    // Save data
+    write_tamogachi(obj, file_path)
 }
